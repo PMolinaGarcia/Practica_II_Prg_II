d8cdae834ca8a5f6958956ec20fa2bba4a6b6d8f
PABLO MOLINA GARCÍA <pablo.molinagarcia1@usp.ceu.es>
2025-02-18 13:52:26 +0100

    Subida javadoc

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,8 +1,15 @@
 package aplicacion;
 import mates.*;
 
+/**
+ * Clase para la ejecución del programa. Al ejecutar por terminal, debemos escribir, a continuación de la ejecución y antes de pulsar "enter", el número de puntos con los que queramos trabajar.
+ */
 public class Principal {
 
+    /**
+     * Método para la ejecución del programa.
+     * @param args Es el parámetro numérico que pasamos por terminal al ejecutar el programa.
+     */
     public static void main(String[] args){
         System.out.println("El número PI es " +
                 Matematicas.generarNumeroPiIterativo(Integer.parseInt(args[0]), 0));
